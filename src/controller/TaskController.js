6059058023c626703d6f497c53866cb56552cfe3
eduardo-sdev <eduardosilvaprogramador@gmail.com>
2022-02-26 15:56:46 -0300

    update

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -3,7 +3,7 @@ const Task = require('../modules/Taks')
 const getAllTasks = async (req, res) => {
     try {
         const tasksList = await Task.find()
-        return res.render('index', {tasksList})
+        return res.render('index', {tasksList, task: null})
     } catch (err) {
         res.status(500).send({error: err.message})
     }
@@ -24,8 +24,31 @@ const createTask = async (req, res) => {
     }
 }
 
+const getById = async (req, res) => {
+    try {
+        const tasksList = await Task.find()
+        const task = await Task.findOne({_id: req.params.id})
+        res.render('index', {task, tasksList})
+    } catch (err) {
+        res.status(500).send({error: err.message})
+    }
+}
+
+const updateOneTask = async (req, res) => {
+    try {
+        const task = req.body
+        await Task.updateOne({_id: req.params.id}, task)
+
+        res.redirect('/')
+    } catch (err) {
+        res.status(500).send({error: err.message})
+    }
+}
+
 module.exports = {
     getAllTasks,
-    createTask
+    createTask,
+    getById,
+    updateOneTask
 }              
                             
